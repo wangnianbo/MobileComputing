dc6b4172e35434ddb86d4ba5f35afa9a0bedbd0b
wangnianbo <wangnianbo@gmail.com>
2016-10-16 16:52:20 +1100

    comments and delete no use class

--- core/src/com/mobilecomputing/game/network/Bluetooth/BluetoothConnection.java
+++ core/src/com/mobilecomputing/game/network/Bluetooth/BluetoothConnection.java
@@ -5,28 +5,45 @@ import java.io.OutputStream;
 import java.util.List;
 
 /**
+ * Bluetooth Connection Interface
  * Created by Bill on 19/09/2016.
  */
 public interface BluetoothConnection {
 
+    /**
+     * Get Client Input Stream
+     * @return
+     */
     public InputStream getClientInputStream();
 
+    /**
+     * Get Client Output Stream
+     * @return
+     */
     public OutputStream getClientOutputStream();
 
+    /**
+     * Get Serve Input Stream
+     * @return
+     */
     public InputStream getServerInputStream();
 
+    /**
+     * Get Server Output Stream
+     * @return
+     */
     public OutputStream getServerOutputStream();
 
+    /**
+     * Wheth it is a net game
+     * @return true if is net game
+     */
     public boolean isNetGame();
 
-    public boolean isBluetoothAvailable();
-
-    public void turnBluetoothOn();
-
-    public List<String> getDeviceList(List<String> deviceList);
-
+    /**
+     * Go to Bluetooth Set Activity!
+     */
     public void gotoBluetoothSet();
 
-    boolean startDiscoverBluetooth();
 
 }
