aa9ccae7eb62c05a94fbc02ed4714bb72a8ffc92
wangnianbo <wangnianbo@gmail.com>
2016-10-16 17:01:17 +1100

    comments

--- android/src/com/mobilecomputing/game/bluetooth/AndroidBluetoothConnection.java
+++ android/src/com/mobilecomputing/game/bluetooth/AndroidBluetoothConnection.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 
 /**
+ * Android Implementation of Bluetooth Connection
  * Created by Bill on 19/09/2016.
  */
 public class AndroidBluetoothConnection   implements BluetoothConnection {
@@ -28,32 +29,54 @@ public class AndroidBluetoothConnection   implements BluetoothConnection {
     }
 
 
-
+    /**
+     * Get Client Input Stream
+     * @return
+     */
     @Override
     public InputStream getClientInputStream() {
         return BluetoothStream.ClientInStream;
     }
 
+    /**
+     * Get Client Output Stream
+     * @return
+     */
     @Override
     public OutputStream getClientOutputStream() {
         return BluetoothStream.ClientOutStream;
     }
 
+    /**
+     * Get Server Input Stream
+     * @return
+     */
     @Override
     public InputStream getServerInputStream() {
         return BluetoothStream.ServerInStream;
     }
 
+    /**
+     * Get Server Output Stream
+     * @return
+     */
     @Override
     public OutputStream getServerOutputStream() {
         return BluetoothStream.ServerOutStream;
     }
 
+    /**
+     * is it a net game
+     * @return
+     */
     @Override
     public boolean isNetGame() {
         return BluetoothStream.isNetGame;
     }
 
+    /**
+     * Switch to Bluetooth connection
+     */
     @Override
     public void gotoBluetoothSet() {
         androidLauncher.gotoBluetoothSet();
